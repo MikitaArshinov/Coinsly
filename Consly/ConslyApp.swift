90ca52c96515abee8428ae7502adc1de941dcac2
Nikita Arshinov <narshinov01@gmail.com>
2023-09-01 15:36:57 +0300

    Initial commit

---
+++ Consly/ConslyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConslyApp.swift
+//  Consly
+//
+//  Created by Nikita Arshinov on 1.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ConslyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Consly/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Consly
+//
+//  Created by Nikita Arshinov on 1.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
